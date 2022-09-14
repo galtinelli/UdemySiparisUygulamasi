f98c109ed077f3d62ad306c16c7ae76d4cfb6887
Gamze Altınelli <gamseacar@windowslive.com>
2022-09-14 17:04:41 +0300

    Add project files.

---
+++ UdemySiparisUygulamasi.Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemySiparisUygulamasi.Data.Repository.IRepository
+{
+    //Generic hale getirilir. T classı temsil eder
+    public interface IRepository<T> where T :class
+    {
+        //Linq sorguları için Expression eklenir
+        T GetFirstOrDefault(Expression<Func<T,bool>> filter, string? includeProperties=null);
+    }
+}
---
+++ UdemySiparisUygulamasi.Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemySiparisUygulamasi.Models
+{
+    public class Cart
+    {
+        //Id yazdığımız için Key yazmamıza gerek yok
+        [Key]
+        public int Id { get; set; }
+
+        //Identityden gelen Id string geliyor
+        public string AppUserId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+
+        //Bir sepet bir kişiye ait olabilir
+
+        [ForeignKey("AppUserId")]
+        public AppUser AppUser { get; set; }
+
+
+        //Bir sepette birden fazla ürün olur
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public Cart()
+        {
+            Count = 1;
+        }
+    }
+}
